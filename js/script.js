77f6a6f351bb28f899ebf8a28f9a2ea6ca4d7043
Acheampong Nana <achiekobby17@gmail.com>
2021-08-01 19:57:53 +0000

    Landing page now responsive

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-const navMenu = document.getElementById("nav")
+const navMenu = document.getElementById("nav");
 
-const menuClose = document.getElementById("menu-close")
+const menuClose = document.getElementById("menu-close");
 
-const menuOpen = document.getElementById("menu-open")
+const menuOpen = document.getElementById("menu-open");
 
-const listItems = document.querySelectorAll(".nav__item")
+const listItems = document.querySelectorAll(".nav__item");
 
-menuOpen.addEventListener('click', ()=>{
-    navMenu.classList.toggle('active')
+menuOpen.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
 });
 
-menuClose.addEventListener('click',()=>{
-    navMenu.classList.remove("active")
-})
+menuClose.addEventListener("click", () => {
+  navMenu.classList.remove("active");
+});
 
-listItems.forEach(item=>item.addEventListener("click",()=>{
-    navMenu.classList.remove('active')
-}));
+listItems.forEach((item) =>
+  item.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  })
+);
+
+// SCROLL REVEAL ANIMATION
+
+const scrollReveal = ScrollReveal({
+  reset: true,
+  duration: 2000,
+  origin: "top",
+  distance: "30px",
+});
+
+scrollReveal.reveal(`.featured, .card, .about, .brand,.contact`);
