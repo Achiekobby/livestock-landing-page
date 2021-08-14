d61f7ee625304d7e0af6f5e85ab39090f22c3dda
Acheampong Nana <achiekobby17@gmail.com>
2021-08-14 19:49:06 +0000

    Navigation bar has been updated

--- js/script.js
+++ js/script.js
@@ -32,13 +32,13 @@ const scrollReveal = ScrollReveal({
 scrollReveal.reveal(`.featured, .card, .about, .brand,.contact`);
 
 // CODE TO TOGGLE BETWEEN ABOUT US BUTTON
-const Btn = document.getElementsByClassName("toggle_menu")
+const Btn = document.getElementsByClassName("toggle_menu");
 
-Btn[0].onclick =  function(event){
-  event.preventDefault()
-  ActiveRemove()
-  this.classList.add("active")
-}
+Btn[0].onclick = function (event) {
+  event.preventDefault();
+  ActiveRemove();
+  this.classList.add("active");
+};
 
 Btn[1].onclick = function (event) {
   event.preventDefault();
@@ -52,22 +52,43 @@ Btn[2].onclick = function (event) {
   this.classList.add("active");
 };
 
-const ActiveRemove = ()=>{
-  for (b of Btn){
-    b.classList.remove("active")
+const ActiveRemove = () => {
+  for (b of Btn) {
+    b.classList.remove("active");
   }
-}
-
+};
 
 // METHOD TO CHANGE THE BACKGROUND OF THE NAVIGATION BAR UPON SCROLL
-const navBar = document.getElementById("navbar")
-const scroll_Nav = ()=>{
-  if(scrollY>=200){
-    navBar.classList.add("scrollNav")
+const navBar = document.getElementById("navbar");
+const scroll_Nav = () => {
+  if (scrollY >= 200) {
+    navBar.classList.add("scrollNav");
+  } else {
+    navBar.classList.remove("scrollNav");
   }
-  else{
-    navBar.classList.remove("scrollNav")
-  }
-}
+};
+window.addEventListener("scroll", scroll_Nav);
 
-window.addEventListener("scroll",scroll_Nav)
+// METHOD TO INDICATE ACTIVE SECTION ON SCROLL
+const sections = document.querySelectorAll("section[id]");
+
+const activeOnScroll = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((currentSection) => {
+    const sectionHeight = currentSection.offsetHeight;
+    const sectionTop = currentSection.offsetTop-200;
+    sectionId = currentSection.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", activeOnScroll);
